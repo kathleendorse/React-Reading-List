0ce202dccadcf9a7cd8ef55e3c929047449bcddf
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-11-19 16:28:19 -0600

    set up deployment

---
+++ client/build/precache-manifest.bdca5d34d44f3db216a7e70996cdf6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7432bd2fb462708b2d030d9e50611ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a455404c4256118c6027",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a455404c4256118c6027",
+    "url": "/static/js/main.ac7a2c82.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
